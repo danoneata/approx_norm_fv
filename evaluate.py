0ea4a70b68ac4b6c5717c771a043ec53521222b6
Dan Oneata <dan.oneata@inria.fr>
2013-07-29 15:44:32 +0200

    Added CLI interface and cleaned the loading code.

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,12 @@
+import argparse
+from ipdb import set_trace
 import numpy as np
 
 from dataset import Dataset
 from fisher_vectors.evaluation import Evaluation
 
 from load_data import load_kernels
+
 
 def print_scores(scores):
     scores = [score * 100 for score in scores]
@@ -12,12 +15,48 @@ def print_scores(scores):
     print "| %.3f" % np.mean(scores)
 
 
-verbose = 10
-dataset = Dataset('hollywood2', suffix='.per_slice.delta_60', nr_clusters=256)
-tr_kernel, tr_labels, te_kernel, te_labels = load_kernels(dataset)
-eval = Evaluation('hollywood2')
-scores = eval.fit(tr_kernel, tr_labels).score(te_kernel, te_labels)
-if verbose > 1:
-    print_scores(scores)
-elif verbose == 1:
-    print "mAP: %.3f" % np.mean(scores)
+def evaluate(tr_norms, te_norms, verbose=0):
+    dataset = Dataset(
+        'hollywood2', suffix='.per_slice.delta_60', nr_clusters=256)
+    (tr_kernel, tr_labels,
+     te_kernel, te_labels) = load_kernels(
+         dataset, tr_norms=tr_norms, te_norms=te_norms)
+
+    eval = Evaluation('hollywood2')
+    scores = eval.fit(tr_kernel, tr_labels).score(te_kernel, te_labels)
+
+    if verbose == 1:
+        print "mAP: %.3f" % np.mean(scores)
+    elif verbose > 1:
+        print 'Train normalizations:', ', '.join(map(str, tr_norms))
+        print 'Test normalizations:', ', '.join(map(str, te_norms))
+        print_scores(scores)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Evaluating the normalization approximations.")
+
+    valid_norms = ['std', 'sqrt', 'L2', 'sqrt_counts']
+
+    parser.add_argument(
+        '--tr_norms', choices=valid_norms, nargs='+',
+        default = [],
+        help="normalizations used for training.")
+    parser.add_argument(
+        '--te_norms', choices=valid_norms, nargs='+',
+        help=("normalizations used for testing "
+             "(by default, use the same as for training)."))
+    parser.add_argument(
+        '-v', '--verbose', action='count', help="verbosity level.")
+
+    args = parser.parse_args()
+
+    if args.te_norms is None:
+        args.te_norms = args.tr_norms
+ 
+    evaluate(args.tr_norms, args.te_norms, verbose=args.verbose)
+
+
+if __name__ == '__main__':
+    main()
--- load_data.py
+++ load_data.py
@@ -9,6 +9,7 @@ from yael import yael
 from fisher_vectors.model.fv_model import FVModel
 from fisher_vectors.model.utils import power_normalize
 from fisher_vectors.model.utils import L2_normalize
+from fisher_vectors.model.utils import sstats_to_sqrt_features
 
 
 def load_sample_data(dataset, sample):
@@ -28,27 +29,61 @@ def load_sample_data(dataset, sample):
     data = np.fromfile(stats_path, dtype=np.float32)
     data = data.reshape(-1, gmm.k * (2 * gmm.d + 1))
     data = FVModel.sstats_to_features(data, gmm)
-
     with open(labels_path, 'r') as ff:
         labels = cPickle.load(ff)
 
     return data, labels
 
 
-def load_kernels(dataset):
+def load_kernels(
+    dataset, tr_norms=['std', 'sqrt', 'L2'], te_norms=['std', 'sqrt', 'L2']):
+
+    # Load sufficient statistics.
     tr_data, tr_labels = load_sample_data(dataset, 'train')
     te_data, te_labels = load_sample_data(dataset, 'test')
 
-    scaler = Scaler()
+    with open(dataset.GMM, 'r') as ff:
+        gmm = yael.gmm_read(ff)
 
-    tr_data = scaler.fit_transform(tr_data)
-    tr_data = power_normalize(tr_data, 0.5)
-    tr_data = L2_normalize(tr_data)
+    scalers = []
+    for norm in tr_norms:
+        if norm == 'std':
+            scaler = Scaler()
+            tr_data = scaler.fit_transform(tr_data)
+            scalers.append(scaler)
+        elif norm == 'sqrt':
+            tr_data = power_normalize(tr_data, 0.5)
+        elif norm == 'sqrt_counts':
+            counts = np.maximum(1e-10, tr_data[:, :gmm.k] + yael.fvec_to_numpy(gmm.w, gmm.k))
+            tr_data = approximate_signed_sqrt(tr_data, counts)  # tr_data[:, :gmm.k])
+        elif norm == 'L2':
+            tr_data = L2_normalize(tr_data)
+
+    ii = 0
+    for norm in te_norms:
+        if norm == 'std':
+            te_data = scalers[ii].transform(te_data)
+            ii += 1
+        elif norm == 'sqrt':
+            te_data = power_normalize(te_data, 0.5)
+        elif norm == 'sqrt_counts':
+            counts = np.maximum(1e-10, te_data[:, :gmm.k] + yael.fvec_to_numpy(gmm.w, gmm.k))
+            te_data = approximate_signed_sqrt(te_data, counts)  # te_data[:, :gmm.k])
+        elif norm == 'L2':
+            te_data = L2_normalize(te_data)
+
     tr_kernel = np.dot(tr_data, tr_data.T)
-
-    te_data = scaler.transform(te_data)
-    te_data = power_normalize(te_data, 0.5)
-    te_data = L2_normalize(te_data)
     te_kernel = np.dot(te_data, tr_data.T)
 
     return tr_kernel, tr_labels, te_kernel, te_labels
+
+
+def approximate_signed_sqrt(data, counts):
+    K = counts.shape[1]
+    N, dim = data.shape
+    D = (dim / K - 1) / 2
+    sqrtQ = np.sqrt(np.abs(counts))
+    sqrt_counts = np.hstack((
+        sqrtQ, np.repeat(sqrtQ, D, axis=1), np.repeat(sqrtQ, D, axis=1)))
+    data[:, :K] = counts
+    return data / sqrt_counts
